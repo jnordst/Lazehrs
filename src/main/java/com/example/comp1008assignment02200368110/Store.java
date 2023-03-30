3b8bf786ce6ee884660af9757bedfc7c9da9780a
Jacob N <levolpehh@hotmail.com>
2023-03-30 19:43:18 -0400

    Created Store.java (Constructor, Getters/Setters, Behaviours)

--- src/main/java/com/example/comp1008assignment02200368110/Store.java
+++ src/main/java/com/example/comp1008assignment02200368110/Store.java
@@ -1,4 +1,34 @@
 package com.example.comp1008assignment02200368110;
 
-public class Store {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store
+{
+    private ArrayList<Product> products;
+
+    public Store() {
+        products = new ArrayList<>();
+    }
+
+    public void addItem(Product product)
+    {
+        products.add(product);
+    }
+
+    public ArrayList<Product> getProducts()
+    {
+        return products;
+    }
+
+    public ArrayList<Product> getProductsFromAisle(int aisle)
+    {
+        return new ArrayList<>(products.stream().filter(product -> product.getAisle() == aisle).toList());
+    }
+
+    public ArrayList<Product> getProductsFromDepartment(String department)
+    {
+        return new ArrayList<>(products.stream().filter(product -> product.getDepartment().equals(department)).toList());
+    }
+
 }
